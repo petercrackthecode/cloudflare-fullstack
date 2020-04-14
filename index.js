cfe6f8bc21d0345f594f44a02be3562ab9ed4c47
petercrackthecode <nguyenvandung02051999@gmail.com>
2020-04-14 05:24:26 -0700

    still trying to resolve the Request problem.

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
+addEventListener("fetch", (event) => {
+  event.respondWith(handleRequest(event.request));
+});
 /**
  * Respond with hello worker text
  * @param {Request} request
  */
 async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
+  const response = await fetch(request.url)
+    .then((response) => {
+      if (response === 200) {
+        return response.json();
+      } else {
+        throw new Error(`An error occured`);
+      }
+    })
+    .catch((error) => {
+      throw new Error(`Hi I am an error: ${error}`);
+    });
+
+  console.log(`response= ${response}`);
+
+  return new Request("abuchikao", {
+    headers: { "content-typel": "text/plain" },
+  });
 }
