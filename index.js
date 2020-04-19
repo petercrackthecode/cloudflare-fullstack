0c122b1af43aa8d6b8aba6cdada343d6bd072140
petercrackthecode <nguyenvandung02051999@gmail.com>
2020-04-19 10:02:41 -0700

    still working on cookies, this thing must be done by tonight.

--- index.js
+++ index.js
@@ -1,64 +1,58 @@
-const url = "https://cfw-takehome.developers.workers.dev/api/variants/";
+const URL = "https://cfw-takehome.developers.workers.dev/api/variants",
+      DEFAULT_EXPIRY_DAYS= 1;
 
 addEventListener("fetch", async (event) => {
   event.respondWith(handleRequest(event.request));
 });
+
+const getVariant = async (variantId) => {
+  return (Math.round(Math.random()) < 0.5) ? (await fetch(URL + `/${variantId}`))
+                                           : (await fetch(URL + `/${variantId}`));
+};
+
+const getNewExpiryDate= (expiryDays) => {
+
+}
+
+const takeVariant= () => {
+  return "hello";
+}
+
+const parseVariantIdFromCookie= (cookie) {
+  if (!cookie) return "";
+
+  
+}
+
+const resetCookie= (cookie) => {
+  cookie= `variant=`
+}
+
+const responseCreator = async (request) => {
+  let cookie= request.headers.get('cookie');
+  let randomId= Math.round(Math.random()) < 0.5 ? 1 : 2;
+  let response;
+  // check if the cookie exists. If it does, parse the cookie and call the variant from that
+  // if cookie exist, we parse it, then reset it to a new expiry date.
+  if (cookie) {
+    cookie= resetCookie(cookie);
+  }
+  else {
+    cookie= `variant=${URL + "/" + randomId}; Expires=${getNewExpiryDate(DEFAULT_EXPIRY_DAYS)}; Path=/`;
+  }
+};
+
 /**
  * Respond with hello worker text
  * @param {Request} request
  */
-
-const randomVariant = (variants) => {
-  return variants[Math.round(Math.random())];
-};
-
-const setCookie = (cookieName, cookieValue, expiredDays) => {
-  let date = new Date();
-  date.setTime(date.getTime() + expiredDays * 24 * 60 * 60 * 1000);
-  let expires = "expires=" + date.toGMTString();
-  document.cookie = cookieName + "=" + cookieValue + ";" + expires + ";path=/";
-};
-
-const resetCookie= (cookieName) => {
-
-};
-
-const getCookie = (cookieName) => {
-  let name = cookieName + "=",
-    decodedCookie = decodeURIComponent(document.cookie),
-    ca = decodedCookie.split(";");
-
-  for (let index = 0; index < ca.length; ++index) {
-    let c = ca[index];
-    while (c.chatAt(0) == " ") {
-      c = c.substring(1);
-    }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
-  }
-
-  return "";
-};
-
-const getVariant = async () => {
-  let variantCookie = getCookie("variant");
-  if (!variantCookie) {
-    const { variants } = await fetch(url).then((res) => res.json());
-    // cookie will expire after 1 day
-    setCookie("variant", randomVariant(variants), 1);
-  } else {
-    resetCookie('variant');
-  }
-};
-
 async function handleRequest(request) {
-  //parse json in variable
-  const { variants } = await fetch(url).then((res) => res.json());
-  let htmlContent = await fetch(randomVariant(variants)).then((res) => res);
+  // const { variants } = await fetch(URL).then((res) => res.json());
 
-  //return 1 or 2 50% of the time
-  return new Response(randomVariant(variants), {
+  const cookie= request.headers.get('cookie');
+  console.log(cookie);
+
+  return new Response(takeVariant(), {
     headers: { "content-type": "text/plain" },
   });
 }
